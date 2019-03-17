c90b14d1d9b868784f3dda072a5120c2b00b288c
Serghei Iakovlev <serghei@phalconphp.com>
2019-03-17 23:41:55 +0200

    Correct documentation to follow code style guide.
    For more see Emacs documentation: D.6 Tips for Documentation Strings

--- bnf-mode.el
+++ bnf-mode.el
@@ -34,28 +34,27 @@
 ;; provides basic syntax and font-locking for BNF files.  BNF notation is
 ;; supported exactly form as it was first announced in the ALGOL 60 report.
 ;;
-;; When developing this mode, the following RFCs were taken into account:
+;; When developing this mode, the following documents were taken into account:
 ;;
-;; - RFC822: Standard for ARPA Internet Text Messages [1]
-;; - RFC5234: Augmented BNF for Syntax Specifications: ABNF [2]
+;; - RFC822: Standard for ARPA Internet Text Messages
+;;   (see URL `https://www.ietf.org/rfc/rfc822.txt')
+;; - RFC5234: Augmented BNF for Syntax Specifications: ABNF
+;;   (see URL `https://www.ietf.org/rfc/rfc5234.txt')
 ;; - FRC7405: Case-Sensitive String Support in ABNF [3]
-;; - Revised Report on the Algorithmic Language Algol 60 [4]
-;;
-;; [1]: https://www.ietf.org/rfc/rfc822.txt
-;; [2]: https://www.ietf.org/rfc/rfc5234.txt
-;; [3]: https://www.ietf.org/rfc/rfc7405.txt
-;; [4]: https://www.masswerk.at/algol60/report.htm
+;;   (see URL `https://www.ietf.org/rfc/rfc7405.txt')
+;; - Revised Report on the Algorithmic Language Algol 60
+;;   (see URL `https://www.masswerk.at/algol60/report.htm')
 ;;
 ;; Usage:  Put this file in your Emacs Lisp path (eg. site-lisp) and add to
 ;; your .emacs file:
 ;;
 ;;   (require 'bnf-mode)
 ;;
-;; Bugs: Bug tracking is currently handled using the GitHub issue tracker at
-;; https://github.com/sergeyklay/bnf-mode/issues
+;; Bugs: Bug tracking is currently handled using the GitHub issue tracker
+;; (see URL `https://github.com/sergeyklay/bnf-mode/issues')
 ;;
 ;; History: History is tracked in the Git repository rather than in this file.
-;; See https://github.com/sergeyklay/bnf-mode/blob/master/CHANGELOG.org
+;; See URL `https://github.com/sergeyklay/bnf-mode/blob/master/CHANGELOG.org'
 
 ;;; Code:
 
